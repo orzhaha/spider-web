7a6c250ecf0aa70030de494154bc1d5cfff724b5
lin-yen <lin-yen@mail.chungyo.net>
2020-03-21 21:11:23 +0800

    add: element

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
